f9432b075711bcb7676f2f0b4b83590f44479a40
ujjwal Shukla <ujjwal.shukla.mec09@itbhu.ac.in>
2015-08-13 15:41:19 +0530

    added Docs

---
+++ api_project.js
@@ -0,0 +1,23 @@
+define({
+  "name": "Edflock-API",
+  "version": "0.0.0",
+  "description": "Api List for the edflock application",
+  "title": "Edflock API Documentation",
+  "url": "http://localhost:8000",
+  "order": [
+    "GetUser",
+    "PostUser"
+  ],
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-08-13T10:10:42.457Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
