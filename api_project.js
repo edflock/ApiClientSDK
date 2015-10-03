46a9666cf5af75b65c3bf4861cee215ebbfde514
ujjwal Shukla <ujjwal.shukla.mec09@itbhu.ac.in>
2015-10-03 22:59:11 +0530

    changed response for add people in class

--- api_project.js
+++ api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-10-03T10:16:27.874Z",
+    "time": "2015-10-03T17:28:29.951Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
