a941d47b9ebe2f67bfd7a4f7d147db91070cb851
ujjwal Shukla <ujjwal.shukla.mec09@itbhu.ac.in>
2015-10-23 18:03:40 +0530

    added register by phone

--- api_project.js
+++ api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-10-23T10:45:56.332Z",
+    "time": "2015-10-23T12:33:20.564Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
